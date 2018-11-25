87bbf40549b17d6f23a3c673a4f7b80795cbcd3a
xfavon <xfavon@163.com>
2018-11-26 00:49:49 +0800

    20181126

--- js/goods_detail.js
+++ js/goods_detail.js
@@ -10,26 +10,20 @@ function init() {
 
 //生成动态轮播图
  function getGoodssilder() {
+     //发送请求
     $.get('goods/detail',{goods_id:getUrl("goods_id")},function (result) { 
-        console.log(result);
+        // console.log(result);
+        //拿到模板数据
         var html=template('goodsliderTmp',result.data);
+        //渲染模板数据
         $('.pyg_view').html(html)
+        //轮播图初始化
         var gallery = mui('.mui-slider');
         gallery.slider({
         interval:2000//自动轮播周期，若为0则不自动播放，默认为0；
         }); 
      })
-
-
-
    }
-
-
-   
-
-
-
-
 
 
 
